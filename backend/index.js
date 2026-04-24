09d29a021a86e302bc507fa81d2ff997a1f641ee
abubakar <itx.abubakar777@gmail.com>
2026-04-23 23:45:09 -0700

    some changes of deployment on vercel

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
+
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +10,16 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 connectDB();
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+// ✅ LOCAL ONLY
+if (process.env.NODE_ENV !== "production") {
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+// ✅ VERCEL
+export default app;
